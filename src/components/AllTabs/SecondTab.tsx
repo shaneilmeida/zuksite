578b3c594206d4e44d304b417bc6bec9bfdf2377
shaneilmeida <134375482+shaneilmeida@users.noreply.github.com>
2024-01-29 12:44:35 +0000

    Update SecondTab.tsx

--- src/components/AllTabs/SecondTab.tsx
+++ src/components/AllTabs/SecondTab.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Grid from "../Grid";
 
-const animations = ["/barubasu clashMK1.mp4"];
+const photos = [
+  "/horror laptop.png",
+  "/ring.png",
+  "/SP gobo.png",
+  "/archer gobo.png",
+  "/pants gobo.png",
+  "/sword gobo.png",
+  "/whole island.png",
+];
 
 const SecondTab = () => {
   return (
     <div className="SecondTab">
-      <Grid items={animations}>Animations</Grid>
+     <Grid items={photos}>all</Grid>
     </div>
   );
 };
