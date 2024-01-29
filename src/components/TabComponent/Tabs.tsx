59fffa2a36959fa11d6b484d4d8b8662321c1deb
shaneilmeida <134375482+shaneilmeida@users.noreply.github.com>
2024-01-29 11:43:27 +0000

    Update Tabs.tsx

--- src/components/TabComponent/Tabs.tsx
+++ src/components/TabComponent/Tabs.tsx
@@ -37,7 +37,7 @@ const Tabs = () => {
         </li>
       </ul>
       <div className="outlet">
-        {activeTab === "tab1" ? <FirstTab /> : <SecondTab /> : <ThirdTab />}
+        {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
       </div>
     </div>
   );
