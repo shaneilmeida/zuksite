56723c8a9ab39be9077971125e8cf31889444d3e
shaneilmeida <134375482+shaneilmeida@users.noreply.github.com>
2024-01-29 11:42:07 +0000

    Update ThirdTab.tsx

--- src/components/AllTabs/ThirdTab.tsx
+++ src/components/AllTabs/ThirdTab.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+import Grid from "../Grid";
 
+
+const ThirdTab = () => {
+  return (
+    <div className='ThirdTab'>
+      <Grid items={photos}>all</Grid>
+    </div>
+  );
+};
+export default ThirdTab;
